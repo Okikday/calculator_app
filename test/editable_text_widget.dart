5c30167702f6cab07da7a28333e9162bcd4ebccb
Okikiola <okikday@gmail.com>
2024-11-14 02:17:15 +0100

    empty commit

--- test/editable_text_widget.dart
+++ test/editable_text_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 enum TextFieldPlacement {
@@ -13,11 +15,11 @@ enum TextFieldPlacement {
 
 class EditableTextWidget {
   // Controllers and Focus Nodes for back and front text fields
-  final TextEditingController backController;
-  final FocusNode backFocusNode;
-  final TextEditingController frontController;
-  final FocusNode frontFocusNode;
-  final ScrollController scrollController;
+  static final TextEditingController backController = TextEditingController();
+  static final FocusNode backFocusNode = FocusNode();
+  static final TextEditingController frontController = TextEditingController();
+  static final FocusNode frontFocusNode = FocusNode();
+  static final ScrollController scrollController = ScrollController();
 
   // Maps and Lists to manage field elements and positioning
   final Map<String, dynamic> frontTextFieldElements = {};
@@ -28,13 +30,9 @@ class EditableTextWidget {
   final double width;
 
   EditableTextWidget({
-    required this.height,
-    required this.width,
-  })  : backController = TextEditingController(),
-        backFocusNode = FocusNode(),
-        frontController = TextEditingController(),
-        frontFocusNode = FocusNode(),
-        scrollController = ScrollController();
+    this.height = 100,
+    this.width = 400,
+  });
 
   // Initialize controllers for managing field elements and positioning
   void createControllers() {}
@@ -45,11 +43,32 @@ class EditableTextWidget {
     required List<TextFieldPlacement> placements,
   }) {}
 
+  void addNormalInput(
+    String content,
+  ) {
+    final TextEditingController controller = backController;
+    int cursorOffset = controller.selection.baseOffset;
+    if (cursorOffset == -1) cursorOffset = controller.text.length;
+
+    controller.text = controller.text.replaceRange(
+      cursorOffset,
+      cursorOffset,
+      content,
+    );
+
+    controller.selection = TextSelection.fromPosition(
+      TextPosition(offset: cursorOffset + content.length),
+    );
+    log("Offset: ${HelperFunctions.measureTextWidth(content, TextStyle(fontSize: height * 0.8, color: Colors.white))}");
+  }
+
   // Deletes content based on cursor position and element positioning
-  void del() {}
+  void delCharacter() {}
 
   // Clears all inputs and resets controllers
-  void clearAll() {}
+  void clearAll() {
+    backController.clear();
+  }
 
   // Tracks and retrieves the text or element at a specified position
   Map<String, dynamic> getTextAtPosition(int position) {
@@ -58,13 +77,24 @@ class EditableTextWidget {
 
   // Back text field (EditableText widget)
   Widget backTextField() {
-    return EditableText(
-      controller: backController,
-      focusNode: backFocusNode,
-      style: TextStyle(fontSize: height * 0.8, color: Colors.black),
-      cursorColor: Colors.blue.withOpacity(0.5),
-      backgroundCursorColor: Colors.white,
-    );
+    return Listener(
+        onPointerDown: (event) {
+          // log("Base offset: ${backController.selection.baseOffset} \nExtent offset: ${backController.selection.extentOffset}");
+        },
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: EditableText(
+            scrollController: scrollController,
+            controller: backController,
+            focusNode: backFocusNode,
+            style: TextStyle(fontSize: height * 0.8, color: Colors.white),
+            cursorColor: Colors.blue.withOpacity(0.5),
+            cursorOpacityAnimates: true,
+            backgroundCursorColor: Colors.white,
+            keyboardType: TextInputType.none,
+          ),
+        ));
   }
 
   // Front text field with positioning functionality
@@ -86,20 +116,71 @@ class EditableTextWidget {
 
   // Container to display layered text fields in a stack
   Widget containerWidget() {
-    return SizedBox(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.lightBlue.withOpacity(0.1),
+      ),
       height: height,
       width: width,
+      clipBehavior: Clip.hardEdge,
       child: Stack(
         children: [
           backTextField(),
-          ListView.builder(
-            controller: scrollController,
-            itemBuilder: (context, index) {
-              return frontTextField();
-            },
-          ),
         ],
       ),
     );
+  }
+}
+
+class TextModel{
+  final String text;
+  final Map<TextFieldPlacement, String>? placementsAndTexts;
+
+  const TextModel({
+    required this.text,
+    this.placementsAndTexts,
+
+  });
+
+  factory TextModel.fromMap(Map<String, dynamic> json){
+    return TextModel(text: json['text'], placementsAndTexts: json['placementsAndTexts']);
+  }
+
+}
+
+class SampleData{
+  List<Map<String, dynamic>> keys = [
+    {
+    'text': "A",
+  },
+  {
+    'text': "B",
+  },
+  {
+    'text': "C",
+    'placementsAndTexts': {
+      TextFieldPlacement.centerLeft: "1",
+      TextFieldPlacement.centerRight: "2"
+    }
+  },
+  {
+    'text': "D",
+    'placementsAndTexts': {
+      TextFieldPlacement.centerLeft: "3",
+      TextFieldPlacement.centerRight: "4"
+    }
+  },
+
+  ];
+}
+
+class HelperFunctions {
+  static double measureTextWidth(String text, TextStyle style) {
+    final TextPainter textPainter = TextPainter(
+      text: TextSpan(text: text, style: style),
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout();
+    return textPainter.size.width;
   }
 }
--- test/main.dart
+++ test/main.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
+import 'package:calculator_app/common/custom_elevated_button.dart';
 import 'package:flutter/material.dart';
+
+import 'editable_text_widget.dart';
 
 void main(){
   runApp(const MyApp());
@@ -10,8 +15,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    
+    final EditableTextWidget editableTextWidget = EditableTextWidget();
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black12,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(child: editableTextWidget.containerWidget()),
+            const SizedBox(height: 24),
+            CustomElevatedButton(onClick: () => editableTextWidget.addNormalInput("A"), label: "Input A", textSize: 16, backgroundColor: Colors.lightBlue,),
+            const SizedBox(height: 16),
+            CustomElevatedButton(onClick: () => editableTextWidget.addNormalInput("B"), label: "Input B", textSize: 16,),
+            const SizedBox(height: 16),
+            CustomElevatedButton(onClick: () => editableTextWidget.clearAll(), label: "Clear textbox", textSize: 16, backgroundColor: Colors.yellow, textColor: Colors.black,)
+          ],
+        ),
+      ),
     );
   }
 }
